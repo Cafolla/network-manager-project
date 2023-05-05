96ba5a179fa94c94fb2709829d7e6926a9f6c87f
Carrick-pre <64624685+Carrick-pre@users.noreply.github.com>
2023-05-05 11:30:22 +0100

    did lots of stuff

---
+++ src/main/java/com/nwm/SeeDb.java
@@ -0,0 +1,20 @@
+package com.nwm;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SeeDb {
+
+    @Autowired
+    JdbcTemplate template;
+
+    @PostConstruct
+    public void init(){
+        template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Athlone",53.425049,-7.944620,"Ericsson Business Park");
+        template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Dublin",53.2025,-6.181184,"PoolBeg");
+        template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Guatemala",14.4200,91.257396,"PoolBeg");
+    }
+}
