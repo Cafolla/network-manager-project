f811890ebdf1ccacf2401bcd2966734f93b2d70a
Nat Cafolla <A00303196@student.ait.ie>
2023-05-05 10:16:22 +0100

    hi

--- src/main/java/com/nwm/Node.java
+++ src/main/java/com/nwm/Node.java
@@ -1,14 +1,73 @@
 package com.nwm;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Node {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
     private String location;
 
     private int latitude;
+
+    private int longitude;
+
+    public Node(int id, String name, String location, int latitude, int longitude) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Node(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
 
 
 }
