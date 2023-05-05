94dc252a958264dcba1eaac630cf2c328b419f2f
Carrick-pre <64624685+Carrick-pre@users.noreply.github.com>
2023-05-05 13:53:30 +0100

    did lots of stuff

--- src/main/java/com/nwm/SeeDb.java
+++ src/main/java/com/nwm/SeeDb.java
@@ -14,7 +14,7 @@ public class SeeDb {
     @PostConstruct
     public void init(){
         template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Athlone",53.425049,-7.944620,"Ericsson Business Park");
-        template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Dublin",53.2025,-6.181184,"PoolBeg");
-        template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Guatemala",14.4200,91.257396,"PoolBeg");
+        template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Dublin",53.2025,-6.200348,"PoolBeg");
+        template.update("insert into NODE(LOCATION,LATITUDE,LONGITUDE,NAME) VALUES (?,?,?,?)","Guatemala",14.4200,-91.264857,"Big swim");
     }
 }
