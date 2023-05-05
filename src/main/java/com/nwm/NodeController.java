596581250ef09671be776102494e1037a3b03f07
Carrick-pre <64624685+Carrick-pre@users.noreply.github.com>
2023-05-05 10:22:53 +0100

    Added Node Controller.

---
+++ src/main/java/com/nwm/NodeController.java
@@ -0,0 +1,30 @@
+package com.nwm;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ericsson")
+@Service
+public class NodeController {
+
+    @PostMapping
+    public ResponseEntity<Node> addNode(@RequestBody Node node){
+    return ResponseEntity.ok()
+    }
+    @GetMapping
+    public ResponseEntity<Iterable<Node>> getAllNodes(){
+
+        return ResponseEntity.ok().body().;
+    }
+
+    public void update(){
+
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> deleteNode(){
+
+        return ResponseEntity.ok().build();
+    }
+}
